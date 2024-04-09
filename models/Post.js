4a8c6e6244743730583ab2602216742fb448ef0a
James Hughes <jameshughes2009@hotmail.com>
2024-04-09 17:23:47 -0400

    working on models

--- models/Post.js
+++ models/Post.js
@@ -2,9 +2,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+class Post extends Model {}
 
-Blog.init(
+Post.init(
     {
         id: {
             type: DataTypes.INTEGER,
@@ -13,37 +13,40 @@ Blog.init(
             autoIncrement: true
         },
         title:{
-            type:String,
-            required:true
-        },
-        authorName:{
-            type:String,
-            allowNull: false
-        },
-        user_id: {
             type: DataTypes.STRING,
+            required:true,
+            validate:{
+                len: [1]
+            },
+        },
+        // authorName:{
+        //     type:String,
+        //     allowNull: false
+        // },
+        user_id: {
+            type: DataTypes.INTEGER,
             references:{
                 model: 'user',
                 key: 'id'
             }
         },
-        description: {
-            type: DataTypes.STRING,
+        story_content: {
+            type: DataTypes.TEXT,
             allowNull: false
         },
-        likes: {
-            type: DataTypes.ARRAY(DataTypes.STRING),
-            defaultValue: []
-        },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+        // likes: {
+        //     type: DataTypes.ARRAY(DataTypes.STRING),
+        //     defaultValue: []
+        // },
+        // category: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false
+        // },
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'blog',
+        modelName: 'post',
     });
 
-module.exports = Blog;
+module.exports = Post;
