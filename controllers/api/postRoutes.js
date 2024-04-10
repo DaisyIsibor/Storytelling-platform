e8c35dc58ba3c6bb431d3b92972d39b351b2cbcc
ashok parihar <aparihar@email.arizona.edu>
2024-04-09 22:20:47 -0600

    Adding POST and DELETE for the user and the post routes.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,2 +1,35 @@
 // Router for all post to get all post.
+const router = require('express').Router();
+const { User, Post, Story  } = require('../../models');
 
+// Creating a new post
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({ ...req.body, user_id: req.session.user_id });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// Deleting a post
+router.delete('/:id', async (req, res) => {
+    try {
+        const storyData = await Comment.destroy({
+            where: { postId: req.params.id },
+        });
+        const postData = await Post.destroy({
+            where: { id: req.params.id, user_id: req.session.user_id, },
+        });
+        if (!postData) {
+        res.status(404).json({
+          message: 'No project found with this id!' });
+        return;
+      }
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+module.exports = router;
