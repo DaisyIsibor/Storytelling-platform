a32d319b62e400e60c000fc83abe9f5e3571b9d6
James Hughes <jameshughes2009@hotmail.com>
2024-04-10 21:50:46 -0400

    adding idividual search function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const sequelize = require('../config/connection');
+const { logger } = require('sequelize/lib/utils/logger');
 
 // Route for the Homepage 
 router.get("/", async (req, res) => {
@@ -19,7 +20,38 @@ router.get("/", async (req, res) => {
 });
 
 // Individual Post 
-
+router.get('/post/:id', async (req, res) => {
+    try{
+        const postData = await Post.findOne({
+            where: {id: req.params.id},
+            attributes:['id', 'content', 'title', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'userId', 'created_at'],
+                    include:{
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        if (postData) {
+            const post = postData.get({plain: true});
+            console.log(post);
+            res.render('single-post', {post, logged_in: req.session.logged_in, username: req.session.username,})
+        } else {
+            res.status(404).json({ message: "No Post with ID"});
+            return;
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
 
 
 //Login 
