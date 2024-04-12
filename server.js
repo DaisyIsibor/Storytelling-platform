8cc8faf6d0962ea493e12ae97b82f272a875547d
ashok parihar <aparihar@email.arizona.edu>
2024-04-11 19:21:28 -0600

    Error checking so we can establish connection.

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const express = require('express');
 const session = require('express-session');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({  });
+// const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
