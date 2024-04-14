b580d1d31bed77b42f11b26e0b650867bb9d8b51
James Hughes <jameshughes2009@hotmail.com>
2024-04-13 22:49:20 -0400

    delete post function

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,1 +1,17 @@
-const deleteButton = document.querySelector()
+const deleteButton = document.querySelector('#del-post-btn');
+const post_id = document.querySelector('input[name="story"]').value; 
+
+const deleteHandler = async () => {
+    const reponse = await fetch(`/api/post/${post_id}`, {
+        method: 'DELETE'
+    });
+    if (reponse.ok) {
+    document.location.replace('/profile');
+    } else {
+        alert(reponse.statusText);
+    }
+};
+
+if(deleteButton!=null){
+    deleteButton.addEventListener('click', deleteHandler);
+}
