a539a8601806c5f506842de69b48fdf324470806
daisyisibor <daisyisibor8@gmail.com>
2024-04-14 11:22:51 -0400

    perfected the hamburger menu and also added max session time on server.js

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
