ec4e9f39e42afd1ef79bfe83b2d3b0ed479819fd
daisyisibor <daisyisibor8@gmail.com>
2024-04-16 17:46:35 -0400

    added theme on post.js  edited handle bars

--- models/User.js
+++ models/User.js
@@ -46,6 +46,8 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+
+      
     },
     sequelize,
     timestamps: false,
